a20a37954bea5930f6e7cdd339aceebc0a3b6599
Dalon <dalon693@gmail.com>
2022-05-03 21:34:44 +0300

    del

--- Assets/Test/MVC/abstract/AnimationView.cs
+++
@@ -1,6 +0,0 @@
-public abstract class AnimationView<T>
-{
-    
-    public abstract void SetAnimation(T animation, bool loop);
-    public abstract void AddAnimation(T animation, bool loop, int trackIndex = 1);
-}
